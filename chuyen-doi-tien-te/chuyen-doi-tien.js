23ee8560e7efafbc367e2912242ae1a66541dd0e
tranminhthong <minhthong9711@gmail.com>
2020-12-09 10:37:37 +0700

    chuyen doi tien

---
+++ chuyen-doi-tien-te/chuyen-doi-tien.js
@@ -0,0 +1,8 @@
+
+function convert() {
+    let mount = document.getElementById('amount').value;
+    let from = document.getElementById('FromCurrency').value;
+    let to = document.getElementById('ToCurrency').value;
+    let result = mount * from / to;
+    document.getElementById('result').innerHTML = result;
+}
